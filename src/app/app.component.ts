31287af0b9097497094856b12fac03a803a3eb94
Chris <chrishughallen@gmail.com>
2026-04-27 10:22:58 +0200

    configure supabase connection and service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { SupabaseService } from './services/supabase.service';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,4 +10,5 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'cible';
+  constructor(private supabase: SupabaseService) {}
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://wzyzjbokogtpipgnvxli.supabase.co',
+  supabaseAnonKey: 'sb_publishable_DQQe-czBZm1QOZM4xY-mZw_c7Wg4px_',
+};
